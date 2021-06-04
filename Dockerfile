657748e8a02369b8f95cb462318952cb40a5b552
praveen <rksby99@gmail.com>
2021-06-04 12:30:47 +0530

    fdr

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM Hello-world
---
+++ varible.tf
@@ -0,0 +1,39 @@
+[8:03:54_pm] [$$ enctrypted $$]
+[8:03:53_pm] [$$ enctrypted $$]
+[8:03:53_pm] [$$ enctrypted $$]
+[8:03:53_pm] [$$ enctrypted $$]
+[8:03:52_pm] [$$ enctrypted $$]
+[8:03:52_pm] [$$ enctrypted $$]
+[8:03:52_pm] [$$ enctrypted $$]
+[8:03:51_pm] [$$ enctrypted $$]
+[8:03:39_pm] [$$ enctrypted $$]
+[8:03:39_pm] [$$ enctrypted $$]
+[8:03:38_pm] [$$ enctrypted $$]
+[8:03:38_pm] [$$ enctrypted $$]
+[8:03:38_pm] [$$ enctrypted $$]
+
+variable "aws_region" {
+	default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+	default = "10.20.0.0/16"
+}
+
+variable "subnets_cidr" {
+	type = "list"
+	default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "azs" {
+	type = "list"
+	default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "webservers_ami" {
+  default = "ami-0ff8a91507f77f867"
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
